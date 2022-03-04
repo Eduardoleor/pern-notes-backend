92cb0a422df96a1ee6687ec9c03bc1ad567c4d21
eduardoleal <eduardo.leal.or@gmail.com>
2022-03-03 20:48:46 -0600

    feat: integrate cors

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
+// TDOO: Dependencies
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
+// TODO: Routes
 const taskRoutes = require('./routes/tasks.routes');
 
+// TODO: App Config
 const app = express();
+
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 
+// TODO: Handle Routes
 app.use(taskRoutes);
 
+// TODO: Listen App
 app.use((err, req, res, next) => {
     return res.status(500).json({
         message: err.message
