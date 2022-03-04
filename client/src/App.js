577963127b8e94405f6a8c0cc062344772fc28c3
eduardoleal <eduardo.leal.or@gmail.com>
2022-03-03 21:10:46 -0600

    feat: integrate new route and material ui for client and config

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import TaskList from './components/TaskList';
+import TaskForm from './components/TaskForm';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<TaskList />} />
+        <Route path="/tasks/new" element={<TaskForm />} />
       </Routes>
     </BrowserRouter>
   );
